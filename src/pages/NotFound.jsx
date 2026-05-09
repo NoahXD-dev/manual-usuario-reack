d2d6a1f2f4774c9543863bf155cfeee0a56a1c06
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-08 22:51:23 -0600

    Enhance NotFound page with improved layout and navigation button

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,21 @@
+import { Button } from 'flowbite-react';
+import { Link } from "react-router-dom";
+import { HiOutlineArrowLeft } from 'react-icons/hi';
+
 export default function NotFound() {
-    return <h1>404</h1>;
+    return (
+        <section>
+            <div className="py-8 px-4 mx-auto lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-sm text-center">
+                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-500">404</h1>
+                    <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Página no encontrada.</p>
+                    <p className="mb-4 text-lg font-light text-gray-400">Lo sentimos, no encontramos lo que buscas.</p>
+                    <Button to="/" as={Link} color="blue" className='inline-flex'>
+                        <HiOutlineArrowLeft className="mr-2 h-5 w-5" />
+                        Volver al inicio
+                    </Button>
+                </div>   
+            </div>
+        </section>
+    )
 }
